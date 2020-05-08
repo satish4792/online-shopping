b0503c781c843c52c4458b2f1ede1b6ee580a7ba
Satish Kumar <satishkumar.iilm@gmail.com>
2020-05-08 19:10:05 +0530

    completed the page and footer issue.

--- onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
@@ -12,8 +12,26 @@ public class PageController {
 	@RequestMapping({ "/", "/home", "/index" })
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", "Welcome to spring web MVC");
+		mv.addObject("title", "Home");
+		mv.addObject("UserClickHome", true);
 		return mv;
 	}
-		
+	
+	
+	@RequestMapping(value="/about")
+	public ModelAndView about() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "About Us");
+		mv.addObject("UserClickAbout", true);
+		return mv;
+	}
+	
+
+	@RequestMapping(value="/contact")
+	public ModelAndView contact() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Contact Us");
+		mv.addObject("UserClickContact", true);
+		return mv;
+	}
 }
